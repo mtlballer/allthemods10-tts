c929efe6ed0600de5425c61be9f87b806d76d8c0
mtlballer <mtlballer@users.noreply.github.com>
2026-03-26 22:15:16 -0400

    Update to 2.0.1
    Update to 2.0.1

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 
 FROM eclipse-temurin:21-jdk
 
-LABEL version="2.0.0"
+LABEL version="2.0.1"
 
 
 RUN apt-get update && apt-get install -y curl unzip jq && \
--- launch.sh
+++ launch.sh
@@ -2,8 +2,8 @@
 set -x
 
 
-NEOFORGE_VERSION=21.1.219
-SERVER_VERSION=2.0.0
+NEOFORGE_VERSION=21.1.221
+SERVER_VERSION=2.0.1
 
 cd /data
 
@@ -17,7 +17,7 @@ fi
 if ! [[ -f "server-$SERVER_VERSION.zip" ]]; then
     rm -fr config defaultconfigs kubejs mods packmenu server-* neoforge*
 
-    curl -Lo "server-$SERVER_VERSION.zip" "https://mediafilez.forgecdn.net/files/7674/266/server-$SERVER_VERSION.zip" || exit 9
+    curl -Lo "server-$SERVER_VERSION.zip" "https://mediafilez.forgecdn.net/files/7820/172/server-$SERVER_VERSION.zip" || exit 9
 
     unzip -u -o "server-$SERVER_VERSION.zip" -d /data
     DIR_TEST="server-$SERVER_VERSION"
